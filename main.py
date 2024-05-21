fde0bb842d1c501715d9796d7f15343e3f95ee40
Rafael Horochovec <rl.horochovec@gmail.com>
2024-05-21 17:28:47 -0300

    Templates funcionais

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask,render_template,request,redirect
-from models import db, MutanteModel
+from models import db,MutanteModel
  
 app = Flask(__name__)
  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///xmen97.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
  
 @app.before_request
@@ -18,12 +17,13 @@ def create():
         return render_template('novo.html')
  
     if request.method == 'POST':
+        mutante_id = request.form['mutante_id']
         nome = request.form['nome']
         codinome = request.form['codinome']
-        mutante = MutanteModel(nome=nome, codinome=codinome)
+        mutante = MutanteModel(mutante_id=mutante_id, nome=nome, codinome=codinome)
         db.session.add(mutante)
         db.session.commit()
-        return redirect('/info')
+        return redirect('/xmen')
  
  
 @app.route('/xmen')
@@ -42,32 +42,31 @@ def detail(id):
  
 @app.route('/xmen/<int:id>/update',methods = ['GET','POST'])
 def update(id):
-    mutante = MutanteModel.query.filter_by(id=id).first()
+    mutante = MutanteModel.query.filter_by(mutante_id=id).first()
     if request.method == 'POST':
         if mutante:
             db.session.delete(mutante)
             db.session.commit()
             nome = request.form['nome']
             codinome = request.form['codinome']
-            mutante = MutanteModel(id=id, nome=nome, codinome=codinome)
+            mutante = MutanteModel(mutante_id=id, nome=nome, codinome=codinome)
             db.session.add(mutante)
             db.session.commit()
             return redirect(f'/xmen/{id}')
         return f"Mutante with id = {id} Does nit exist"
  
-    return render_template('editar.html', mutante = mutante)
+    return render_template('edita.html', mutante = mutante)
  
  
 @app.route('/xmen/<int:id>/delete', methods=['GET','POST'])
 def delete(id):
-    mutante = MutanteModel.query.filter_by(id=id).first()
+    mutante = MutanteModel.query.filter_by(mutante_id=id).first()
     if request.method == 'POST':
         if mutante:
             db.session.delete(mutante)
             db.session.commit()
             return redirect('/xmen')
-         
  
-    return render_template('delete.html')
+    return render_template('deleta.html')
  
 app.run(host='localhost', port=5000)
--- models.py
+++ models.py
@@ -3,16 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
  
 class MutanteModel(db.Model):
-    __tablename__ = "mutante"
+    __tablename__ = "mutantes"
  
     id = db.Column(db.Integer, primary_key=True)
+    mutante_id = db.Column(db.Integer(),unique = True)
     nome = db.Column(db.String())
     codinome = db.Column(db.String())
  
-    def __init__(self, id,nome,codinome):
-        self.id = id
+    def __init__(self, mutante_id,nome,codinome):
+        self.mutante_id = mutante_id
         self.nome = nome
         self.codinome = codinome
  
     def __repr__(self):
-        return f"{self.nome}:{self.id}"
+        return f"{self.nome}:{self.mutante_id}"
